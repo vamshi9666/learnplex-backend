19eadde620a1c86deb2dfbeece4c1c58b1bd37e6
Bhanu Teja P <pbteja1998@gmail.com>
2020-03-21 16:22:15 +0530

    Adds ADMIN middleware for createTopic

--- src/modules/resource/CreateTopic.resolver.ts
+++ src/modules/resource/CreateTopic.resolver.ts
@@ -1,10 +1,14 @@
-import { Arg, Mutation, Resolver } from 'type-graphql'
+import { Arg, Mutation, Resolver, UseMiddleware } from 'type-graphql'
 
 import { Topic } from '../../entity/Topic.entity'
 import { CreateTopicInput } from './topic/CreateTopicInput'
+import { isAuthorized } from '../middleware/isAuthorized'
+import { hasRole } from '../middleware/hasRole'
+import { UserRole } from '../../entity/enums/UserRole.enum'
 
 @Resolver()
 export class CreateTopicResolver {
+  @UseMiddleware(isAuthorized, hasRole([UserRole.ADMIN]))
   @Mutation(() => Topic)
   async createTopic(@Arg('data') { title }: CreateTopicInput): Promise<Topic> {
     return Topic.create({ title, resources: Promise.resolve([]) }).save()
