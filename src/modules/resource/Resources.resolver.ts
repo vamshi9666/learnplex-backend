7128a3f2378c795949ab52d7f0a1e4693961e642
Bhanu Teja P <pbteja1998@gmail.com>
2020-03-23 04:50:52 +0530

    Modifies the resources resolver to return only currentUser's resources

--- src/modules/resource/Resources.resolver.ts
+++ src/modules/resource/Resources.resolver.ts
@@ -1,11 +1,15 @@
-import { Query, Resolver } from 'type-graphql'
+import { Query, Resolver, UseMiddleware } from 'type-graphql'
 
 import { Resource } from '../../entity/Resource.entity'
+import { isAuthorized } from '../middleware/isAuthorized'
+import CurrentUser from '../../decorators/currentUser'
+import { User } from '../../entity/User.entity'
 
 @Resolver()
 export class ResourcesResolver {
+  @UseMiddleware(isAuthorized)
   @Query(() => [Resource])
-  async resources(): Promise<Resource[]> {
-    return Resource.find()
+  async resources(@CurrentUser() currentUser: User): Promise<Resource[]> {
+    return currentUser.resources
   }
 }
