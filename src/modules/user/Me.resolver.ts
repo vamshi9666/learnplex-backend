127370c3b3a436b0c489dbdf6370300fc7667dbb
Bhanu Teja P <pbteja1998@gmail.com>
2020-04-25 13:31:45 +0530

    Change the return value of the MeQuery

--- src/modules/user/Me.resolver.ts
+++ src/modules/user/Me.resolver.ts
@@ -5,21 +5,18 @@ import { User } from '../../entity/User.entity'
 import { isAuthorized } from '../middleware/isAuthorized'
 import { UserRole } from '../../entity/enums/UserRole.enum'
 import { hasRole } from '../middleware/hasRole'
+import { LoginResponse } from './login/LoginResponse'
+import { createAccessToken } from '../../utils/auth'
 
 @Resolver()
 export class MeResolver {
   @UseMiddleware(isAuthorized, hasRole([UserRole.USER]))
-  @Query(() => User, { nullable: true })
-  async me(@Ctx() { payload }: MyContext): Promise<User | undefined> {
-    try {
-      const [user] = await User.find({
-        where: { id: payload!.userId },
-        take: 1,
-      })
-      return user
-    } catch (e) {
-      console.error(e)
-      return undefined
-    }
+  @Query(() => LoginResponse)
+  async me(@Ctx() { payload }: MyContext): Promise<LoginResponse> {
+    const [user] = await User.find({
+      where: { id: payload!.userId },
+      take: 1,
+    })
+    return { accessToken: createAccessToken(user.id), user }
   }
 }
