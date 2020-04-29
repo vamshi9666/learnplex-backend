4afe3a5acccca8d4e6b7a42564f906ae5198ff5b
Bhanu Teja P <pbteja1998@gmail.com>
2020-04-29 22:00:20 +0530

    Automatically refresh access token if refresh_token is available

--- src/modules/middleware/isAuthorized.ts
+++ src/modules/middleware/isAuthorized.ts
@@ -2,7 +2,43 @@ import { MiddlewareFn } from 'type-graphql'
 import { verify } from 'jsonwebtoken'
 
 import { MyContext } from '../../types/MyContext'
-import { JWTAuthPayload } from '../../utils/auth'
+import {
+  createAccessToken,
+  createRefreshToken,
+  JWTAuthPayload,
+  sendAccessToken,
+  sendRefreshToken,
+} from '../../utils/auth'
+import { User } from '../../entity/User.entity'
+
+export const refreshToken = async (context: MyContext) => {
+  let payload: JWTAuthPayload
+  try {
+    const refreshToken =
+      context.req.cookies[`${process.env.REFRESH_COOKIE_NAME}`]
+    payload = verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET!
+    ) as JWTAuthPayload
+  } catch (e) {
+    throw new Error('not authenticated')
+  }
+  const [user] = await User.find({ where: { id: payload.userId }, take: 1 })
+
+  if (!user) {
+    console.error(`User ${payload.userId} is not defined`)
+    throw new Error('not authenticated')
+  }
+
+  if (user.tokenVersion !== payload.tokenVersion) {
+    console.error(`token version mismatch for user ${user.id}`)
+    throw new Error('not authenticated')
+  }
+
+  sendRefreshToken(context.res, createRefreshToken(user))
+  sendAccessToken(context.res, createAccessToken(user.id))
+  return payload
+}
 
 export const getAuthorizationPayloadFromToken = (context: MyContext) => {
   let payload
@@ -12,7 +48,11 @@ export const getAuthorizationPayloadFromToken = (context: MyContext) => {
     payload = verify(token, process.env.ACCESS_TOKEN_SECRET!) as JWTAuthPayload
   } catch (e) {
     console.error(e)
-    throw new Error('not authenticated')
+    try {
+      payload = refreshToken(context)
+    } catch (e) {
+      throw new Error('not authenticated')
+    }
   }
 
   return payload
